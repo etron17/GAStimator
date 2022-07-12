c7ee0d6abf621f18e01e75cb12ceebea8abd1788
BigDO <kim408@mcmaster.ca>
2022-07-12 09:03:42 -0400

    Calling vehicle data from Clever cloud database

---
+++ car.py
@@ -0,0 +1,53 @@
+import mysql.connector
+from typing import Dict, List
+
+
+class Car(object):
+    def __init__(self, brand, model, drive, fuel, stnd, city, hwy) -> None:
+        super().__init__()
+        self.__brand = brand
+        self.__model = model
+        self.__drive = drive
+        self.__fuel = fuel
+        self.__stnd = stnd
+        self.__city = city
+        self.__hwy = hwy
+
+    def __repr__(self) -> str:
+        return "'" + self.__brand + ", " + self.__model + "," + self.__drive + \
+               "," + self.__fuel + "," + self.__stnd + \
+               ", Fuel efficiency: City " + str(self.__city) + \
+               "mpg and highway " + str(self.__hwy) + "mpg'"
+
+
+def read_vehicles() -> Dict[str, List[Car]]:
+    """
+    Read data from database and return as dictionary, the key is car producer,
+    values are cars from that producer
+    :return: Dictionary
+    """
+    cars: Dict[str, List[Car]] = {}
+
+    # Connect to database
+    mydb = mysql.connector.connect(
+        host="YOUR KEY",
+        user="YOUR KEY",
+        passwd="YOUR KEY",
+        database="YOUR KEY"
+    )
+
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT * FROM vehicledb")
+    myresult = mycursor.fetchall()
+
+    print(myresult)
+
+    # Fill the dictionary
+    for row in myresult:
+        brand = row[0]
+        car = Car(brand, row[1], row[2], row[3], row[4], row[5], row[6])
+        if brand not in cars:
+            cars[brand] = []
+        cars[brand].append(car)
+
+    return cars
