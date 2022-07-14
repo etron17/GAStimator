1c3cbd343e3a646311c66076706a8a8dde6d8eb4
BigDO <kim408@mcmaster.ca>
2022-07-14 09:46:13 -0400

    Implement MVC pattern

--- routing.py
+++ routing.py
@@ -1,25 +1,28 @@
 import requests
 from typing import Tuple
 
-# API key
-api_key = "YOUR API KEY"
 
-# Base URL
-url = "YOUR URL"
+# MVC pattern - model part
+class Routing(object):
+    @staticmethod
+    def find_dest(departure, destination) -> Tuple[int, float]:
+        """
+        Find destination between 2 points
+        :param departure: Departure point
+        :param destination: Destination point
+        :return: Tuple with time and km between 2 points
+        """
+        # API key
+        api_key = "YOUR API KEY"
 
-def find_dest(departure, destination) -> Tuple[int, float]:
-    """
-    Get ETA between departure and destination
-    :param departure: Departure point
-    :param destination: Destination point
-    :return: Tuple with time and distance between 2 points
-    """
+        # Base URL
+        url = "YOUR URL"
 
-    # Get response
-    r = requests.get(
-        url + "orignins=" +departure + "&destinations=" + destination + "&key=" + api_key)
+        # get response
+        r = requests.get(
+            url + "origins=" + departure + "&destinations=" + destination + "&key=" + api_key)
 
-    time = r.json()["rows"][0]["elements"][0]["duration"]["text"]
-    distance = r.json()["rows"][0]["elements"][0]["distance"]["value"]
+        time = r.json()["rows"][0]["elements"][0]["duration"]["text"]
+        distance = r.json()["rows"][0]["elements"][0]["distance"]["value"]
 
-    return (time, distance)
+        return (time, distance)
